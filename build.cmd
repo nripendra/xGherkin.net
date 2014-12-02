6f82d771d5d13224ab388df8903321df35e5f02b
nripendra <nripendra@uba-solutions.com>
2014-12-03 00:43:26 +0545

    copy nuget.exe to .nuget folder

--- build.cmd
+++ build.cmd
@@ -1,4 +1,11 @@
+REM update nuget
 "tools\nuget\nuget.exe" "update" "-self"
+
+REM install Fake.Core
 "tools\nuget\nuget.exe" "install" "FAKE.Core" "-OutputDirectory" "tools" "-ExcludeVersion" "-version" "3.9.9"
 
+REM copy updated nuget to .nuget folder
+copy "tools\nuget\nuget.exe" ".nuget\nuget.exe" /y
+
+REM Build
 "tools\FAKE.Core\tools\Fake.exe" "build.fsx"
